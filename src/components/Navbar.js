14e4947b56548c709c89802368b6b151483876d1
“svkcreations” <thesvk1@gmail.com>
2024-12-14 18:38:27 +0530

    PATH 02

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,14 +7,14 @@ export default function Navbar() {
     <div>
       <nav className="navbar navbar-expand-lg">
   <div className="container-fluid p-0 m-0">
-    <Link className="navbar-brand d-flex align-items-center justify-content-center gap-2 fw-bold" style={{color:'#365c1e'}} to="/"><img src={logo} className="d-block mx-lg-auto img-fluid" alt="Bootstrap Themes" width='30' loading="lazy"/>USinfinity</Link>
+    <Link className="navbar-brand d-flex align-items-center justify-content-center gap-2 fw-bold" style={{color:'#365c1e'}} to="/usinfinity/"><img src={logo} className="d-block mx-lg-auto img-fluid" alt="Bootstrap Themes" width='30' loading="lazy"/>USinfinity</Link>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
     <div className="collapse navbar-collapse" id="navbarSupportedContent">
       <ul className="navbar-nav me-auto mb-2 mb-lg-0">
         <li className="nav-item">
-          <Link className="nav-link active" aria-current="page" to="/">Home</Link>
+          <Link className="nav-link active" aria-current="page" to="/usinfinity/">Home</Link>
         </li>
         <li className="nav-item">
           <Link className="nav-link" to="/about">About us</Link>
