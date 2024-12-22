2b8f6c37b72f5353f661efafad092371a485d5e3
“svkcreations” <thesvk1@gmail.com>
2024-12-22 21:50:49 +0530

    pics added

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,6 +3,7 @@ import logo from '../images/Logo.png'
 import { Link, useLocation } from "react-router-dom";
 import { Icon } from '@iconify/react/dist/iconify.js';
 
+
 export default function Navbar() {
   const location = useLocation()
 
@@ -17,27 +18,27 @@ export default function Navbar() {
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
               <li className="nav-item">
-                <Link className={location.pathname === '/usinfinity/'? "active nav-link": "nav-link" } aria-current="page" to="/usinfinity/">Home</Link>
+                <Link className={location.pathname === '/usinfinity/' ? "active nav-link" : "nav-link"} aria-current="page" to="/usinfinity/">Home</Link>
               </li>
               <li className="nav-item">
-                <Link className={location.pathname === '/about'? "active nav-link": "nav-link" } to="/about">About us</Link>
+                <Link className={location.pathname === '/about' ? "active nav-link" : "nav-link"} to="/about">About us</Link>
               </li>
               <li className="nav-item">
-                <Link className={location.pathname === '/journey'? "active nav-link": "nav-link" } to="/journey">Our Journey</Link>
+                <Link className={location.pathname === '/journey' ? "active nav-link" : "nav-link"} to="/journey">Our Journey</Link>
               </li>
               <li className="nav-item">
-                <Link to='/gallery' className={location.pathname === '/gallery'? "active nav-link": "nav-link" }>Our Gallery</Link>
+                <Link to='/gallery' className={location.pathname === '/gallery' ? "active nav-link" : "nav-link"}>Our Gallery</Link>
               </li>
               <li className="nav-item">
-                <Link to='/playlist' className={location.pathname === '/playlist'? "active nav-link": "nav-link" }>Our Playlist</Link>
+                <Link to='/playlist' className={location.pathname === '/playlist' ? "active nav-link" : "nav-link"}>Our Playlist</Link>
               </li>
               <li className="nav-item">
-                <Link to='/countdown' className={location.pathname === '/playlist'? "active nav-link": "nav-link" }>Countdown</Link>
-              </li>
-              <li className="nav-item">
-                <Link to='/quiz' className="d-flex align-items-center p-2 gap-1 quiz-link mx-md-3 my-2 my-md-0"><Icon icon="mage:light-bulb" width="24" height="24" />Take a Quiz</Link>
+                <Link to='/countdown' className={location.pathname === '/countdown' ? "active nav-link" : "nav-link"}>Countdown</Link>
               </li>
             </ul>
+            <div class="d-flex" role="search">
+              <Link to='/quiz' className="d-flex align-items-center p-2 gap-1 quiz-link mx-md-3 my-2 my-md-0"><Icon icon="mage:light-bulb" width="24" height="24" />Take a Quiz</Link>
+            </div>
           </div>
         </div>
       </nav>
